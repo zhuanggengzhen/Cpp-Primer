1dc79722de3771cccc3ae07ad227e8889cdcd927
zhuanggengzhen <630153122@qq.com>
2015-10-31 12:48:50 -0700

    initializer_list assignment operator for class StrVec

---
+++ chapter14/ex14_23.h
@@ -0,0 +1,31 @@
+#include <memory>
+#include <string>
+
+class StrVec
+{
+public:
+	StrVec() : elements(nullptr), first_free(nullptr), cap(nullptr){}
+	StrVec(const StrVec&);
+	StrVec(StrVec&&) noexcept;
+	StrVec(std::initializer_list<std::string>);
+	StrVec &operator=(const StrVec&);
+	StrVec &operator=(StrVec&&) noexcept;
+	StrVec &operator=(std::initializer_list<std::string>);
+	~StrVec();
+	void push_back(const std::string&);
+	size_t size() const { return first_free - elements; }
+	size_t capacity() const { return cap - elements; }
+	std::string *begin() const { return elements; }
+	std::string *end() const { return first_free; }
+
+private:
+	std::pair<std::string*, std::string*> alloc_n_copy(const std::string*, const std::string*);
+	void free();
+	void chk_n_alloc() { if (size() == capacity()) reallocate(); }
+	void reallocate();
+private:
+	std::string *elements;
+	std::string *first_free;
+	std::string *cap;
+	static std::allocator<std::string> alloc;
+};
