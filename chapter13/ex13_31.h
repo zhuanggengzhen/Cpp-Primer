aad891b78967cf4afb2b78c8c9a368980d056a29
zhuanggengzhen <630153122@qq.com>
2015-10-30 23:24:31 -0700

    ex13_31

---
+++ chapter13/ex13_31.h
@@ -0,0 +1,35 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+class HasPtr
+{
+	friend bool operator<(const HasPtr &lhs, const HasPtr &rhs);
+public:
+	HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0) {}
+	HasPtr(const HasPtr &hp) : ps(new std::string(*(hp.ps))), i(hp.i) { }
+	HasPtr &operator=(const HasPtr &rhs)
+	{
+		std::string *new_ps = new std::string(*rhs.ps);
+		delete ps;//To avoid memory leaks
+		ps = new_ps;
+		i = rhs.i;
+		return *this;
+	}
+	void swap(HasPtr &lhs, HasPtr &rhs)
+	{
+		std::cout << "my swap\n";
+		using std::swap;
+		swap(lhs.ps, rhs.ps);
+		swap(lhs.i, rhs.i);
+	}
+	~HasPtr(){ delete ps; }
+private:
+	std::string *ps;
+	int i;
+};
+
+bool operator<(const HasPtr &lhs, const HasPtr &rhs)
+{
+	return *lhs.ps < *rhs.ps;
+}
