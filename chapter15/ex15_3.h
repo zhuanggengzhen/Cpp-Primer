c03297aa6f7ba9974854a4eef0beda1a94bae98e
zhuanggengzhen <630153122@qq.com>
2015-11-15 00:57:48 -0800

    class Quote

--- chapter15/ex15_3.h
+++ chapter15/ex15_3.h
@@ -6,6 +6,25 @@ class Quote
 public:
 	Quote() = default;
 	Quote(const std::string &book, double sales_price) : bookNo(book), price(sales_price) { }
+	Quote(const Quote &quote) : bookNo(quote.bookNo), price(quote.price) { std::cout << "copy constructor\n"; }
+	Quote(Quote &&quote) noexcept : bookNo(std::move(quote.bookNo)), price(std::move(quote.price)) { std::cout << "move constructor\n"; }
+	Quote &operator=(const Quote &rhs)
+	{
+		std::cout << "copy=()\n";
+		bookNo = rhs.bookNo;
+		price = rhs.price;
+		return *this;
+	}
+	Quote &operator=(Quote &&rhs) noexcept
+	{ 
+		std::cout << "move=()\n";
+		if(*this != rhs)
+		{
+			bookNo = rhs.book;
+			price = rhs.price;
+		}
+		return *this;
+	}
 	std::string isbn() const { return bookNo; }
 	virtual double net_price(std::size_t n) const { return n * price; }
 	virtual ~Quote() = default;
